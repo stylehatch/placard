dc721c6a16b742a37bd60aa0cb208f2fae2ff6cb
Jonathan Moore <jonathan@stylehatch.co>
2013-03-13 12:55:32 -0700

    Setting up empty placard JS and CSS files

---
+++ js/placard.js
@@ -0,0 +1,10 @@
+/**
+ * placard - v0.9.0
+ * 2013-03-13
+ * Display details on a website as a fixed overlay bar and modal window
+ * http://stylehatch.github.com/photoset-grid/
+ *
+ * Copyright 2013 Jonathan Moore, @moore - Style Hatch
+ */
+
+ /*jshint browser: true, curly: true, eqeqeq: true, forin: false, immed: false, newcap: true, noempty: true, strict: true, undef: true, devel: true */
