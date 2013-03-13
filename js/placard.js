30f57103397e01d4790ac843413356b3a43ce1d5
Jonathan Moore <jonathan@stylehatch.co>
2013-03-13 14:53:53 -0700

    Simple starter script to inject the placard HTML

--- js/placard.js
+++ js/placard.js
@@ -7,4 +7,30 @@
  * Copyright 2013 Jonathan Moore, @moore - Style Hatch
  */
 
- /*jshint browser: true, curly: true, eqeqeq: true, forin: false, immed: false, newcap: true, noempty: true, strict: true, undef: true, devel: true */
+ /*jshint smarttabs:true, browser: true, curly: true, eqeqeq: true, forin: false, immed: false, newcap: true, noempty: true, strict: true, undef: true, devel: true */
+ /*global $:false */
+( function( window, undefined ) {
+
+'use strict';
+
+// dependencies
+var StyleHatch = window.StyleHatch;
+
+function loadPlacard(){
+  // console.log('The placard has been loaded!');
+
+  // Temporary HTML to inject
+  var modalHTMl = '<div class="stylehatch-placard cleanslate">' +
+                    '<div class="logo"><a href="http://stylehatch.co" target="_blank"><img src="http://static.tumblr.com/fzgsfsd/Lqcm27cej/style-hatch.png" width="56" height="36"></a></div>' +
+                    '<div class="theme"><h6>Customization Mode</h6><h2>' + StyleHatch.themeName + '</h2></div>' +
+                    '<div class="links"><a href="' + StyleHatch.demoSiteURL + '" class="modal-btn demo" target="_blank">Full Demo</a><a href="' + StyleHatch.supportURL + '" class="modal-btn support" target="_blank">Theme Support</a></div>' +
+                  '</div>';
+
+  $('body').append( modalHTMl );
+
+  return true;
+}
+
+StyleHatch.loadPlacard = loadPlacard;
+
+})( window );
